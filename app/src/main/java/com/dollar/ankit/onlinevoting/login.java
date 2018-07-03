8d882b1f45e11764e20fa09c0dcfaf8545547ac8
Ankit-lnmiit <ankit131296@gmail.com>
2018-07-03 17:39:11 +0530

    My second commit

--- app/src/main/java/com/dollar/ankit/onlinevoting/login.java
+++ app/src/main/java/com/dollar/ankit/onlinevoting/login.java
@@ -1,13 +1,14 @@
 package com.dollar.ankit.onlinevoting;
 
 import android.content.Intent;
+import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.StrictMode;
+//import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
+//import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
@@ -17,160 +18,158 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
+//import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.Map;
+//import java.util.Map;
 
 public class login extends AppCompatActivity {
 
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+    //private View mProgressView;
+    //private View mLoginFormView;
     TextView testDisplay;
     FirebaseFirestore db;
-    Map<String, Object> user;
+    //Map<String, Object> user;
     CollectionReference cities;
-    DocumentReference docRef;
+    //DocumentReference docRef;
     Query query;
-    public static boolean loginStatus1;
-    public static final String MyPREFERENCES = "MyPrefs" ;
+    //public static boolean loginStatus1;
+    public static final String MyPREFERENCES = "MyPrefs";
     public static final String Name = "nameKey";
     public static final String Email = "emailKey";
     public static final String loginStatus = "loginKey";
-    SharedPreferences sharedpreferences;
+    //SharedPreferences sharedpreferences;
+    static String name;
+    static String email;
+    //String email2;
+    static String type = "default";
+    //static boolean cancel = true;
 
-    //String temp, temp2;
-//    private MongoClient mongoClient;
-//    private MongoDatabase database;
-//    private MongoCollection<Document> collection;
-//    private FindIterable<Document> findIterable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Intent intent = getIntent();
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
-        testDisplay = findViewById((R.id.sampleText));
 
-        mEmailView = (android.widget.AutoCompleteTextView) findViewById(R.id.email);
+//        if (android.os.Build.VERSION.SDK_INT > 21) {
+//            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//            StrictMode.setThreadPolicy(policy);
+//        }
+
+
+        mEmailView = findViewById(R.id.email);
 
     }
 
     public void authentication(View view) {
-        mEmailView = (android.widget.AutoCompleteTextView) findViewById(R.id.email);
-        mPasswordView = (EditText) findViewById(R.id.pass);
+
+        mPasswordView = findViewById(R.id.pass);
         db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
+        email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
-
-        final boolean[] cancel = new boolean[2];
         View focusView;
-        final String[] temp = new String[2];
 
         // Check for a valid email address.
-        boolean checklnmiitemail= email.endsWith("lnmiit.ac.in");
+        boolean checklnmiitemail = email.endsWith("lnmiit.ac.in");
 
         if (!isEmailValid(email)) {
             mEmailView.setError(getString(R.string.error_invalid_email));
             focusView = mEmailView;
             //cancel = true;
             focusView.requestFocus();
-            return;
-        }
-        else if (TextUtils.isEmpty(email)) {
+        } else if (TextUtils.isEmpty(email)) {
             mEmailView.setError(getString(R.string.empty));
             focusView = mEmailView;
             //cancel = true;
             focusView.requestFocus();
-            return;
-        }
-        else if(!checklnmiitemail){
+        } else if (!checklnmiitemail) {
             mEmailView.setError(getString(R.string.invalid_lnmiit_email));
             focusView = mEmailView;
             //cancel = true;
             focusView.requestFocus();
-            return;
-        }
-        else if (TextUtils.isEmpty(password)) {
+        } else if (TextUtils.isEmpty(password)) {
             mPasswordView.setError(getString(R.string.empty));
             focusView = mPasswordView;
             focusView.requestFocus();
             //cancel = true;
-            return;
-        }
-        else if(!isPasswordValid(password)){
-                mPasswordView.setError(getString(R.string.error_invalid_password));
-                focusView = mPasswordView;
-                //cancel = true;
-                focusView.requestFocus();
-                return;
-        }
-        else{
+        } else if (!isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            //cancel = true;
+            focusView.requestFocus();
+        } else {
             cities = db.collection("users");
-            query = cities.whereEqualTo("email",email).whereEqualTo("password",password);
-
+            query = cities.whereEqualTo("email", email).whereEqualTo("password", password);
             query.get()
                     .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
                             if (task.isSuccessful()) {
+                                String temp, temp2;
+                                SharedPreferences sharedpreferences;
                                 for (QueryDocumentSnapshot document : task.getResult()) {
-                                    temp[0] = document.get("name").toString();
-                                    temp[1] = document.get("type").toString();
-                                    cancel[0] = false;
+                                    if (document != null) {
+                                        temp = document.getString("name");
+                                        temp2 = document.getString("type");
+
+                                        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                                        editor.putString(Name, temp);
+                                        editor.putString(loginStatus, "true");
+                                        editor.putString(Email, email);
+                                        editor.apply();
+
+                                        if (temp2.equals("voter")) {
+                                            Intent intent;
+                                            intent = new Intent(login.this, user.class);
+                                            startActivity(intent);
+                                        } else if (temp2.equals("admin")) {
+                                            Intent intent;
+                                            intent = new Intent(login.this, admin.class);
+                                            startActivity(intent);
+                                        }
+                                        else{
+                                            Toast.makeText(login.this,"ERROR",Toast.LENGTH_SHORT).show();
+                                        }
+
+                                    } else {
+                                            Toast.makeText(login.this,"User Not Registered or wrong password",Toast.LENGTH_SHORT).show();
+                                    }
                                 }
                             } else {
-                                Toast.makeText(login.this, "ERROR" + R.string.email_not_found,
-                                        Toast.LENGTH_SHORT).show();
-                                cancel[0] = true;
-                                Log.d("sample2", "Error getting documents: ", task.getException());
+                                //boolean cancel2;
+                                Toast.makeText(login.this,"ERROR",Toast.LENGTH_SHORT).show();
+
+                                //Log.d("sample2", "Error getting documents: ", task.getException());
                             }
                         }
                     });
-
-            if(cancel[0]){
-                return;
-            }
-            else{
-                SharedPreferences.Editor editor = sharedpreferences.edit();
-                editor.putString(Name, temp[0]);
-                editor.putString(loginStatus, "true");
-                editor.putString(Email, email);
-                editor.commit();
-
-                if(temp[1].equals("voter")) {
-                    Intent intent = new Intent(this, user.class);
-                    loginStatus1=true;
-                    startActivity(intent);
-                }
-                else if(temp[1].equals("admin")) {
-                    Intent intent = new Intent(this, admin.class);
-                    loginStatus1=true;
-                    startActivity(intent);
-                }
-
-            }
         }
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
+
         return email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
+
         return password.length() > 4;
     }
 }
+
+
+
